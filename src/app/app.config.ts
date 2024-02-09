6067faa4e3388960538bfbe7e3381e282e0d5e9f
Artiom Iurcovschi <artiom.08@gmail.com>
2024-02-09 19:17:24 +0200

    add app initializer service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,15 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { APP_CONFIG, APP_SERVICE_CONFIG } from './AppConfig/appconfig.service';
 import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { requestInterceptor } from './request.interceptor';
+import { InitService } from './init.service';
+
+function initFactory(initService: InitService) {
+  return () => initService.init();
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,6 +21,12 @@ export const appConfig: ApplicationConfig = {
     {
       provide: APP_SERVICE_CONFIG,
       useValue: APP_CONFIG,
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initFactory,
+      deps: [InitService],
+      multi: true,
     },
   ],
 };
